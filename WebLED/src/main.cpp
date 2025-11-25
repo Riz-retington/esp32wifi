574f54f97ba1a28f049b4e99cd50ee73ba951319
Ron <sr1666@example.com>
2025-11-25 11:37:41 +0800

    src code OK

---
+++ WebLED/src/main.cpp
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+#include <WebServer.h>
+#define LED 2
+
+const char *ssid = "Riz";
+const char *password = "0903989352";
+const byte Led= 2;
+WebServer server(80);
+
+void handleRoot(){
+  String HTML = "<a href=\"/on\">開啟LED</a> <br>";
+  HTML += "<a href=\"/on\">關閉LED</a>";
+  server.send(200, "text/html; charset=UTF-8", HTML);
+}
+
+void handleNotFound(){
+  server.send(404, "text/plain; charset=UTF-8", "找不到檔案");
+}
+
+void handleLedOn(){
+ digitalWrite(LED, HIGH);
+}
+
+void handleLedOff(){
+  digitalWrite(LED, LOW);
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  pinMode(LED, OUTPUT);
+
+  while(WiFi.status()!=WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi Connected!!");
+  Serial.print("My IP = ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/",handleRoot);
+  server.on("/on",handleLedOn);
+  server.on("/off",handleLedOff);
+  server.onNotFound(handleNotFound);
+  server.begin();
+  Serial.println("Server is running...");
+}
+
+void loop() {
+  server.handleClient();
+}
---
+++ webserver/src/main.cpp
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+#include <WebServer.h>
+
+const char *ssid = "Riz";
+const char *password = "0903989352";
+
+WebServer server(80);
+
+void handleRoot(){
+  server.send(200, "text/html; charset=UTF-8", "Hello World");
+}
+
+void handleNotFound(){
+  server.send(404, "text/plain; charset=UTF-8", "找不到檔案");
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  While(WiFi.status()!=WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi Connected!!");
+  Serial.print("My IP = ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/",handleRoot);
+  server.onNotFound("/",handleNotFound);
+  server.begin();
+  Serial.println("Server is running...");
+}
+
+void loop() {
+  server.handleClient();
+}
